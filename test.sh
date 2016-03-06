1bebc7440707ee3f7c58a6c739cd5c2ea5cbcd9e
cloud-worker-lv7 <cloud.worker.lv7@gmail.com>
2016-03-06 23:47:09 +0900

    ffffffff

--- test.sh
+++ test.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-
-OPT=`getopt -o "" -l target: -l env: -- "$@"`
-
 echo ${OPT}
-
-if [ "$?" -ne 0 ]; then
-  error "Usage: $0 [--target VALUE] [--env VALUE]"
-fi
-eval set -- "$OPT"
-until [ "$1" == "--" ]; do
-echo $1
-  case $1 in
-    --target)
-      target=$2
-      ;;
     --env)
       env=$2
       ;;
